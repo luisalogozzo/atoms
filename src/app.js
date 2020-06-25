e6dae6f43945138cbae2d40056c96e257031dac9
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-06-25 16:55:05 +0200

    js/jquery

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
 
-  $('.nav-right').on('click', 'button', function(e) {
-    $('.button').removeClass('active');
-    $(e.target).addClass('active');
+  $('.nav-right').on('click', 'a', function(e) {
+    $('.nav-right a').removeClass('active-header');
+    if (!$(e.target).hasClass('button')) {
+      $(e.target).addClass('active-header');
+    }
+  });
+
+  $('.cross').on('click', function() {
+    $('.newsletter-box').hide();
+  });
+
+  $('.services-row .h4').on('click', function(e) {
+    $('.services-row .h4 h4').removeClass('active-main');
+    $(this).find('h4').addClass('active-main');
+    $('.services-row .h4').siblings('.line').find('svg').removeClass('active-line');
+    $(this).siblings('.line').find('svg').addClass('active-line');
+  });
+
+  $('.lang a').on('click', function(e) {
+    $('.lang a').removeClass('active-lang');
+    $(this).addClass('active-lang');
   });
 
 });
