60aba69f035d519bbe8fa054520bf8a74c7f2f35
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-06-27 14:50:39 +0200

    up

--- dist/app.js
+++ dist/app.js
@@ -104,11 +104,13 @@ $(document).ready(function () {
   $('.cross').on('click', function () {
     $('.newsletter-box').hide();
   });
-  $('.services-row .h4').on('click', function (e) {
+  $('.services-row').on('click', function (e) {
     $('.services-row .h4 h4').removeClass('active-main');
+    $('.services-row .p p').removeClass('active-main');
     $(this).find('h4').addClass('active-main');
+    $(this).find('p').addClass('active-main');
     $('.services-row .h4').siblings('.line').find('svg').removeClass('active-line');
-    $(this).siblings('.line').find('svg').addClass('active-line');
+    $(this).find('.line').find('svg').addClass('active-line');
   });
   $('.lang a').on('click', function (e) {
     $('.lang a').removeClass('active-lang');
--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ $(document).ready(function() {
     $('.newsletter-box').hide();
   });
 
-  $('.services-row .h4').on('click', function(e) {
+  $('.services-row').on('click', function(e) {
     $('.services-row .h4 h4').removeClass('active-main');
+    $('.services-row .p p').removeClass('active-main');
     $(this).find('h4').addClass('active-main');
+    $(this).find('p').addClass('active-main');
     $('.services-row .h4').siblings('.line').find('svg').removeClass('active-line');
-    $(this).siblings('.line').find('svg').addClass('active-line');
+    $(this).find('.line').find('svg').addClass('active-line');
   });
 
   $('.lang a').on('click', function(e) {
